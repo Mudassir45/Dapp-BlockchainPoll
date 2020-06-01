354adfa093b3dfb9ea38967c102279a402464c70
Mudassir Ali <mudassirali36@gmail.com>
2020-06-01 23:14:54 +0500

    completed all components for Front-End

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Poll, PollForm, PollVote } from './poll-create/types';
+import { PollService } from './poll-service/poll.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blockchain-poll';
+  showForm = false;
+  activePoll: Poll = null;
+
+  polls = this.ps.getPolls();
+
+  constructor(private ps: PollService) {
+
+  }
+
+  setActivePoll(poll) {
+    this.activePoll = null;
+
+    setTimeout(() => {
+      this.activePoll = poll;
+    }, 100);
+  }
+
+  handlePollCreate(poll: PollForm) {
+    this.ps.createPoll(poll);
+  }
+
+  handlePollVote(pollVoted: PollVote) {
+    this.ps.vote(pollVoted.id, pollVoted.vote);
+  }
 }
